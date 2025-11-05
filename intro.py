ee9d1cfeedeb457df80794e3ab3bdbb807f3a46f
Antik-123 <antikmahmud99@gmail.com>
2025-11-05 13:28:01 +0600

    first commit

---
+++ intro.py
@@ -0,0 +1,7 @@
+#type conversion
+
+a = int("2")
+b=  4.32
+
+print(a + b)
+print(type(a))
---
+++ lesson1.py
@@ -0,0 +1,40 @@
+#string is a data type that store a sequesnce of characters
+
+str1 = "Antik"
+
+# print(len(str1))
+
+#indexing
+
+# print(str1[1])
+
+# str1[2] = 'b'
+
+# print(str1[2])
+
+#slicing................................................
+
+#it means basically accesing parts of a string
+
+# print(str1[1:3])
+
+#negative index
+
+# print(str1[-1])
+
+#string function
+
+#str.endswith("")___retrurns true if string ends with substr
+str = "i am studing python"
+
+# print(str.endswith("on"))
+
+#str.capitalize("")______capitalizes 1st char
+
+
+# print(str.capitalize())
+
+#str.repalce(old, new)____replace all occurences of old value to new value
+
+
+
